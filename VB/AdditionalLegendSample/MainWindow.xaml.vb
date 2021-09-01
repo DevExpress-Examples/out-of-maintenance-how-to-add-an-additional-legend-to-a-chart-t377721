7fd7af7b3d63d9a042e734174d4b2930213cf0be
DevExpressExampleBot <fakeEmail>
2021-09-01 18:28:50 +0300

    Source auto update [skip ci]

--- VB/AdditionalLegendSample/MainWindow.xaml.vb
+++ VB/AdditionalLegendSample/MainWindow.xaml.vb
@@ -17,44 +17,44 @@ Imports System.Windows.Resources
 Imports System.Windows.Shapes
 
 Namespace AdditionalLegendSample
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            DataContext = LoadData()
-        End Sub
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			DataContext = LoadData()
+		End Sub
 
-        Private Function LoadData() As List(Of DataPoint)
-            Dim result As New List(Of DataPoint)()
-            Dim info As StreamResourceInfo = Application.GetResourceStream(New Uri("Data/AUDUSDDaily.csv", UriKind.RelativeOrAbsolute))
-            Dim reader As New StreamReader(info.Stream)
-            Do While Not reader.EndOfStream
-                Dim line As String = reader.ReadLine()
-                Dim values = line.Split(","c)
-                Dim point = New DataPoint()
-                point.Argument = Date.ParseExact(values(0), "yyyy.MM.dd", Nothing)
-                point.OpenValue = Double.Parse(values(1), CultureInfo.InvariantCulture)
-                point.HighValue = Double.Parse(values(2), CultureInfo.InvariantCulture)
-                point.LowValue = Double.Parse(values(3), CultureInfo.InvariantCulture)
-                point.CloseValue = Double.Parse(values(4), CultureInfo.InvariantCulture)
-                result.Add(point)
-            Loop
-            Return result
-        End Function
-    End Class
+		Private Function LoadData() As List(Of DataPoint)
+			Dim result As New List(Of DataPoint)()
+			Dim info As StreamResourceInfo = Application.GetResourceStream(New Uri("Data/AUDUSDDaily.csv", UriKind.RelativeOrAbsolute))
+			Dim reader As New StreamReader(info.Stream)
+			Do While Not reader.EndOfStream
+				Dim line As String = reader.ReadLine()
+				Dim values = line.Split(","c)
+				Dim point = New DataPoint()
+				point.Argument = DateTime.ParseExact(values(0), "yyyy.MM.dd", Nothing)
+				point.OpenValue = Double.Parse(values(1), CultureInfo.InvariantCulture)
+				point.HighValue = Double.Parse(values(2), CultureInfo.InvariantCulture)
+				point.LowValue = Double.Parse(values(3), CultureInfo.InvariantCulture)
+				point.CloseValue = Double.Parse(values(4), CultureInfo.InvariantCulture)
+				result.Add(point)
+			Loop
+			Return result
+		End Function
+	End Class
 
-    Public Class DataPoint
-        Public Property Argument() As Date
-        Public Property LowValue() As Double
-        Public Property HighValue() As Double
-        Public Property OpenValue() As Double
-        Public Property CloseValue() As Double
-    End Class
+	Public Class DataPoint
+		Public Property Argument() As DateTime
+		Public Property LowValue() As Double
+		Public Property HighValue() As Double
+		Public Property OpenValue() As Double
+		Public Property CloseValue() As Double
+	End Class
 End Namespace
